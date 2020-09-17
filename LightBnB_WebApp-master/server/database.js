eea3d9db90f0479caa578dab9e2eb43ca82e77b0
Peteami <peteami@hotmail.com>
2020-09-17 19:02:44 +0000

    Refactored the getAllReservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -140,18 +140,43 @@ exports.addUser = addUser;
 // ----------------------------------------
 /// Reservations
 
+// /**
+//  * Get all reservations for a single user.
+//  * @param {string} guest_id The id of the user.
+//  * @return {Promise<[{}]>} A promise to the reservations.
+//  */
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// }
+// exports.getAllReservations = getAllReservations;
+
+
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*, properties.*, avg(property_reviews.rating)
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.reservation_id = reservations.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY reservations.id, properties.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => {
+    return console.log(err);
+  })
 }
 exports.getAllReservations = getAllReservations;
-
-
-
 
 
 
