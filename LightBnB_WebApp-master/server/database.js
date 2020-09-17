82c44e12eb0afa0e4bc556133e63a51f93bd619a
Peteami <peteami@hotmail.com>
2020-09-17 22:42:25 +0000

    cleaned up the code

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -11,27 +11,6 @@ const pool = new Pool({
 
 
 // ----------------------------------------
-/// Users
-// /**
-//  * Get a single user from the database given their email.
-//  * @param {String} email The email of the user.
-//  * @return {Promise<{}>} A promise to the user.
-//  */
-// const getUserWithEmail = function(email) {
-//   let user;
-//   for (const userId in users) {
-//     user = users[userId];
-//     if (user.email.toLowerCase() === email.toLowerCase()) {
-//       break;
-//     } else {
-//       user = null;
-//     }
-//   }
-//   return Promise.resolve(user);
-// }
-// exports.getUserWithEmail = getUserWithEmail;
-
-
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
@@ -58,20 +37,7 @@ exports.getUserWithEmail = getUserWithEmail;
 
 
 
-
-
 // -------------------------------------------
-// /**
-//  * Get a single user from the database given their id.
-//  * @param {string} id The id of the user.
-//  * @return {Promise<{}>} A promise to the user.
-//  */
-// const getUserWithId = function(id) {
-//   return Promise.resolve(users[id]);
-// }
-// exports.getUserWithId = getUserWithId;
-
-
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
@@ -94,22 +60,7 @@ exports.getUserWithId = getUserWithId;
 
 
 
-
-
 // ------------------------------------------------
-// /**
-//  * Add a new user to the database.
-//  * @param {{name: string, password: string, email: string}} user
-//  * @return {Promise<{}>} A promise to the user.
-//  */
-// const addUser =  function(user) {
-//   const userId = Object.keys(users).length + 1;
-//   user.id = userId;
-//   users[userId] = user;
-//   return Promise.resolve(user);
-// }
-// exports.addUser = addUser;
-
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
@@ -136,18 +87,6 @@ exports.addUser = addUser;
 
 // ----------------------------------------
 /// Reservations
-
-// /**
-//  * Get all reservations for a single user.
-//  * @param {string} guest_id The id of the user.
-//  * @return {Promise<[{}]>} A promise to the reservations.
-//  */
-// const getAllReservations = function(guest_id, limit = 10) {
-//   return getAllProperties(null, 2);
-// }
-// exports.getAllReservations = getAllReservations;
-
-
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
@@ -178,36 +117,6 @@ exports.getAllReservations = getAllReservations;
 
 
 // ------------------------------------------------------
-
-/// Properties
-
-/**
-//  * Get all properties.
-//  * @param {{}} options An object containing query options.
-//  * @param {*} limit The number of results to return.
-//  * @return {Promise<[{}]>}  A promise to the properties.
-//  */
-// const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
-// exports.getAllProperties = getAllProperties;
-
-
-// const getAllProperties = function(options, limit = 10) {
-//   return pool.query(`
-//   SELECT * FROM properties
-//   LIMIT $1
-//   `, [limit])
-//   .then(res => res.rows);
-// }
-// exports.getAllProperties = getAllProperties;
-
-
-// ------------------updated-----------
 const getAllProperties = function(options, limit = 10) {
   const queryParams = [];
   let queryString = `
@@ -259,21 +168,9 @@ const getAllProperties = function(options, limit = 10) {
 }
 exports.getAllProperties = getAllProperties;
 
+
+
 // ---------------------------------------------------------------
-// /**
-//  * Add a property to the database
-//  * @param {{}} property An object containing all of the property details.
-//  * @return {Promise<{}>} A promise to the property.
-//  */
-// const addProperty = function(property) {
-//   const propertyId = Object.keys(properties).length + 1;
-//   property.id = propertyId;
-//   properties[propertyId] = property;
-//   return Promise.resolve(property);
-// }
-// exports.addProperty = addProperty;
-
-
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
@@ -293,35 +190,3 @@ const addProperty = function(property) {
 exports.addProperty = addProperty;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// SELECT properties.*, avg(property_reviews.rating) as average_rating
-//   FROM properties
-//   JOIN property_reviews ON properties.id = property_id
-//   WHERE city LIKE '%Ottawa%' AND cost_per_night >= '1' AND cost_per_night <= '500' 
-//   GROUP BY properties.id
-  
-//   ORDER BY cost_per_night
-//   LIMIT 20;
-
-
-
-
-
-// SELECT properties.cost_per_night FROM properties
-//   JOIN property_reviews ON properties.id = property_id
-//   WHERE city LIKE '%Ottawa%' AND cost_per_night <= '50000000' 
-//   ORDER BY cost_per_night DESC
-//   LIMIT 20;
-
- 
